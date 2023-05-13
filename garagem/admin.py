09509f947514d9b233d8747238b0c0dd510905a0
Elias <souzabrancoelias8@gmail.com>
2023-05-13 20:55:48 -0300

    Criaçãpo do Acessorio, Cor e Modelo

--- garagem/admin.py
+++ garagem/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 
 from django.contrib import admin
 
-from .models import Marca
+from .models import Acessorio
 from .models import Categoria
+from .models import Cor 
+from .models import Marca
+from .models import Modelo
 
-admin.site.register(Marca)
+admin.site.register(Acessorio)
 admin.site.register(Categoria)
+admin.site.register(Cor)
+admin.site.register(Marca)
+admin.site.register(Modelo)
--- garagem/models.py
+++ garagem/models.py
@@ -13,3 +13,35 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.descricao
+
+class Acessorio(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.descricao} ({self.id})"
+
+    class Meta:
+        verbose_name = "acessório"
+        verbose_name_plural = "acessórios"
+ 
+   
+class Cor(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.descricao}"
+
+    class Meta:
+        verbose_name = "cor"
+        verbose_name_plural = "cores"
+
+
+class Modelo(models.Model):
+    descricao = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"{self.descricao} ({self.id})"
+
+    class Meta:
+        verbose_name = "modelo"
+        verbose_name_plural = "modelos"
