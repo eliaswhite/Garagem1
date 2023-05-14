9e9a5097326ee1b10eba427baa5d6d34dd335fce
Elias <souzabrancoelias8@gmail.com>
2023-05-13 21:09:47 -0300

    Criação do Veículo

--- garagem/admin.py
+++ garagem/admin.py
@@ -7,9 +7,11 @@ from .models import Categoria
 from .models import Cor 
 from .models import Marca
 from .models import Modelo
+from .models import Veiculo
 
 admin.site.register(Acessorio)
 admin.site.register(Categoria)
 admin.site.register(Cor)
 admin.site.register(Marca)
 admin.site.register(Modelo)
+admin.site.register(Veiculo)
--- garagem/models.py
+++ garagem/models.py
@@ -45,3 +45,25 @@ class Modelo(models.Model):
     class Meta:
         verbose_name = "modelo"
         verbose_name_plural = "modelos"
+        
+class Veiculo(models.Model):
+    marca = models.ForeignKey(
+        Marca, on_delete=models.PROTECT, related_name="veiculos"
+    )
+    modelo = models.CharField(max_length=15, null=False, default=0)
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="veiculos"
+    )
+    cor = models.ForeignKey(
+        Cor, on_delete=models.PROTECT, related_name="veiculos"
+    )
+    ano = models.IntegerField(null=True,default=0)
+    preco = models.DecimalField(max_digits=10, decimal_places=5, null=True, 
+    default=0)
+
+    def __str__(self):
+        return f"Marca: {self.marca} Modelo: {self.modelo} Ano: {self.ano} Cor: {self.cor}"
+    
+    class Meta:
+        verbose_name = "veículo"
+        verbose_name_plural = "veículos"
