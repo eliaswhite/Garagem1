1148b4eebc6342af09aa6b0f1e8baddca65daf2d
Elias <souzabrancoelias8@gmail.com>
2023-05-13 21:42:54 -0300

    Criação dos endpoints da Cor

--- config/urls.py
+++ config/urls.py
@@ -3,11 +3,12 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from garagem.views import MarcaViewSet, CategoriaViewSet
+from garagem.views import MarcaViewSet, CategoriaViewSet, CorViewSet
 
 router = DefaultRouter()
 router.register(r"marcas", MarcaViewSet)
 router.register(r"categorias", CategoriaViewSet)
+router.register(r"cores", CorViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
